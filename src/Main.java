ea80c0e149c3bbb6505d221103ad9a2a329ece4c
manavpatnaik <manavpatnaik@hotmail.com>
2020-11-05 14:37:47 +0530

    Changed main class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        User user = new User("John", 19);
-        System.out.println(user.getName());
-        System.out.println(user.getAge());
+        System.out.println("Hello World");
     }
 }
