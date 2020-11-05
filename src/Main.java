f417900654a4639be946de59f82769e820e0db88
manavpatnaik <manavpatnaik@hotmail.com>
2020-11-05 14:35:25 +0530

    CReated Main and User classes

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("John", 19);
+        System.out.println(user.getName());
+        System.out.println(user.getAge());
+    }
+}
